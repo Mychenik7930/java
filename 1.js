17a8e8f0ad9e08be001a1f32d8337b4e2a7f4ce7
Mychenik7930 <gamerdax.play@gmail.com>
2025-01-20 16:16:24 +0900

    1 СДЕЛАЛ

---
+++ 1.js
@@ -0,0 +1,8 @@
+const myName = 'Maksim';
+const programmingLanguage = 'pascal';
+const courseCreatorName = 'ausheev';
+const reasonText = 'ne znayou'
+const numberOfMonth = '12'
+const result1 = `Всем привет! Меня зовут, ${myName}. Сейчас я изучаю язык программирования ${programmingLanguage} на кyрсе по ${programmingLanguage} y ${courseCreatorName}.
+Я хочу стать веб-разработчиком, потому что ${reasonText}. До этого я изучал(а) ${programmingLanguage} ${numberOfMonth} месяцев. Я уверен(а), что пройдуданный курс до конца!`
+console.log(result1)
