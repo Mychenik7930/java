ef1a17a6b9b963460d86a84e4f0a443f1ec33036
Mychenik7930 <gamerdax.play@gmail.com>
2025-01-26 23:09:55 +0900

    4 задани

---
+++ 4.js
@@ -0,0 +1,7 @@
+const userAnswer = prompt('КАК ВАС ЗОВУТ');
+const username = userAnswer;
+username.toUpperCase(username);
+const AgeAnswer = prompt('сколько вам лит');
+let age = Number(AgeAnswer);
+alerting = (`првиет ${username.toUpperCase(username).replace(/\s+/g, '')} тебе ${age}`);
+console.log(alert(alerting,))
