5b330a6bcb76e412e753dcbf6e21e355d80a414c
Mychenik7930 <gamerdax.play@gmail.com>
2025-01-26 23:00:48 +0900

    доделал 3 задани

--- 3.js
+++ 3.js
@@ -1,5 +1,5 @@
 const userAnswer = prompt('КАК ВАС ЗОВУТ');
 const username = userAnswer;
 username.toUpperCase(username);
-alerting = (`првиет ${username.toUpperCase(username)}`);
+alerting = (`првиет ${username.toUpperCase(username).replace(/\s+/g, '')}`);
 console.log(alert(alerting,))
