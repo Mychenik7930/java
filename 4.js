62dc9db80ad49b3e6df1490ec76d71616262c4fc
Mychenik7930 <gamerdax.play@gmail.com>
2025-01-27 14:47:40 +0900

    жесть капец

--- 4.js
+++ 4.js
@@ -1,7 +1,22 @@
-const userAnswer = prompt('КАК ВАС ЗОВУТ');
+/* const userAnswer = prompt('КАК ВАС ЗОВУТ');
 const username = userAnswer;
-username.toUpperCase(username);
+username.toUpperCase(username).trim();
 const AgeAnswer = prompt('сколько вам лит');
 let age = Number(AgeAnswer);
-alerting = (`првиет ${username.toUpperCase(username).replace(/\s+/g, '')} тебе ${age}`);
-console.log(alert(alerting,))
+alerting = (`првиет ${username.toUpperCase(username)} тебе ${age}`);
+console.log(alert(alerting,)) */
+
+const name1 = 'Никита'
+
+let sredniisimvol = Math.floor(name1.length / 2)
+let BUKVATUPAYA = name1[sredniisimvol]
+let bukvaumnaya = name1[name1.length -1]
+let bukvaumnaya1 = name1[name1.length -2]
+const futurebukva = prompt("кака буква в будущм бцдкт")
+let reversetxt = name1.split('').reverse().join()
+alert(sredniisimvol)
+alert(BUKVATUPAYA)
+alert(bukvaumnaya)
+alert(bukvaumnaya1)
+alert(name1 +futurebukva)
+alert(reversetxt)
