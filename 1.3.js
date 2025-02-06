e756e8dd2a66490150460de058a28dd654bc7cb9
Mychenik7930 <gamerdax.play@gmail.com>
2025-02-06 12:16:25 +0900

    задани куча

---
+++ 1.3.js
@@ -0,0 +1,20 @@
+const question1 = confirm("JavaScript появился в 1995 году?");
+if (question1) {
+    alert("Верно");
+} else {
+    alert("ты глупы JavaScript рил появился в 1995 году.");
+}
+
+const question2 = confirm("Спецификация JavaScript называется ECMAScript????");
+if (question2) {
+    alert("Верно");
+} else {
+    alert("Неверно. Спецификация JavaScript действительно называется ECMAScript.");
+}
+
+const question3 = confirm("JavaScript был создан за 1 месяц?");
+if (question3) {
+    alert("Верно");
+} else {
+    alert("ой бляя JavaScript рил был создан за рекордно короткий срок — примерно за 10 дней в мае 1995 года.");
+}
